f4c8c1e50ced83abd26c12a56f5a464f52cc2237
jin-333 <manimani.knock31@icloud.com>
2024-10-15 22:32:13 +0900

    databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-            $this->call[
+            $this->call([
                 CategorySeeder::class,
                 PrefectureSeeder::class,
-            ];
+            ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
